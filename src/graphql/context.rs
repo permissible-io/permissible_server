deea67ae6f93682d9b91a41546bcea6db57d8c8e
Oliver Wright <meiamsome@meiamso.me>
2019-08-17 15:27:43 +0100

    Use a JWT header for authentication

--- src/graphql/context.rs
+++ src/graphql/context.rs
@@ -1,9 +1,11 @@
 use diesel::r2d2::{ConnectionManager, Pool};
 use diesel::sqlite::SqliteConnection;
+use uuid::Uuid;
 
 pub struct Context {
     pub user_id: Option<i32>,
     pub database: Pool<ConnectionManager<SqliteConnection>>,
+    pub request_uuid: Uuid,
 }
 
 impl juniper::Context for Context {}
